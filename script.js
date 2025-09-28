0d811f91b2d97ab97d71241f9873ce0287d2f09c
AdrianodevSP <dklszdev@gmail.com>
2025-09-28 22:57:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const downloadButton = document.getElementById('downloadButton');
+
+    downloadButton.addEventListener('click', () => {
+        // Updated: Download is Coming Soon.
+        alert('NebulaX is currently undergoing final quality assurance. Download link will be available soon!');
+    });
+});
